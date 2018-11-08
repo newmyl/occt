ad4261a904240fb864a14e2f6aed30a9b49ac36c
nds <nds@opencascade.com>
2018-11-08 16:04:05 +0300

    Correction for a regression cased by "0030268: Inspectors - improvements in VInspector plugin"

--- src/Quantity/Quantity_Color.cxx
+++ src/Quantity/Quantity_Color.cxx
@@ -14,7 +14,6 @@
 // commercial license or contractual agreement.
 
 #include <Quantity_Color.hxx>
-#include <Quantity.hxx>
 
 #include <Quantity_ColorDefinitionError.hxx>
 #include <Standard_ErrorHandler.hxx>
@@ -48,8 +47,6 @@ static Standard_Real TheEpsilon = 0.0001;
 Standard_Boolean Quantity_Color::ColorFromName (const Standard_CString theName,
                                                 Quantity_NameOfColor&  theColor)
 {
-  return Quantity::NameOfColorFromString (theName, theColor);
-  /*
   TCollection_AsciiString aName (theName);
   aName.UpperCase();
   if (aName.Search("QUANTITY_NOC_") == 1)
@@ -66,7 +63,7 @@ Standard_Boolean Quantity_Color::ColorFromName (const Standard_CString theName,
       return Standard_True;
     }
   }
-  return Standard_False;*/
+  return Standard_False;
 }
 
 Quantity_Color::Quantity_Color () {
@@ -2043,8 +2040,6 @@ switch (AName) {
 Standard_CString Quantity_Color::StringName (
                                  const Quantity_NameOfColor AName) {
 
-  return Quantity::NameOfColorToString (AName);
-/*
 Standard_CString TheName;
  
 switch (AName) {
@@ -3605,7 +3600,7 @@ switch (AName) {
 		throw Standard_OutOfRange("Bad name");
 	break;
 }
-return (TheName);*/
+return (TheName);
 }
 
 //
